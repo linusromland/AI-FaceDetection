6c469a74d25a8f0884cf38691cdceeb5bb97f1d4
Linus Romland <romlandlinus@gmail.com>
2020-12-04 23:12:15 +0100

    working as i want

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const video = document.getElementById("video");
+let hold = document.getElementById("hold");
+let mood = document.getElementById("mood");
+let accurancy = document.getElementById("accurat");
+
+let closest;
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
@@ -28,10 +33,54 @@ video.addEventListener("play", () => {
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
       .withFaceExpressions();
-    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    /*const resizedDetections = faceapi.resizeResults(detections, displaySize);
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     faceapi.draw.drawDetections(canvas, resizedDetections);
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);*/
+    let expression = checkExpression(detections[0].expressions);
+    hold.innerText = "The AI thinks you mood is:";
+    mood.innerText = (await expression).expression
+    accurancy.innerText = " and is " + (await expression).accuracy + "% certain about it!"
+
+
   }, 100);
 });
+
+//This is the worst code i have written in quite a while. I hate it. And will change it. I am just very tired and did not want to think. So this is the answer to that
+async function checkExpression(expression) {
+  let numbers = [
+    expression.angry,
+    expression.disgusted,
+    expression.fearful,
+    expression.happy,
+    expression.neutral,
+    expression.sad,
+    expression.surprised,
+  ];
+  let expressions = [
+    "angry",
+    "disgusted",
+    "fearful",
+    "happy",
+    "neutral",
+    "sad",
+    "surprised",
+  ];
+
+  closest = numbers.reduce((a, b) => {
+      return Math.abs(b - 1) < Math.abs(a - 1) ? b : a;
+  });
+  
+  let largest = numbers.findIndex(ifRight)
+  let accurat = (numbers[largest]*100).toFixed(1)
+  return {"expression": expressions[largest], "accuracy": accurat}
+}
+
+function ifRight(input) {
+  return closest == input;
+}
+
+
+
+
